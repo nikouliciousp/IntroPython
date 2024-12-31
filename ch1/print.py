4f56909386b8d3e396db497a7da068cdead5dd9a
perisnik <nikouliciousp@gmail.com>
2024-12-31 07:10:40 +0200

    v1.4

--- ch1/print.py
+++ ch1/print.py
@@ -18,15 +18,36 @@ print("My name is %s and I am %d years old." % (name, age))
 # Print with custom separator
 print("Python", "is", "awesome", sep="-")
 
+# Demonstrating sep argument with different separators
+print("Red", "Green", "Blue", sep=", ")
+print("1", "2", "3", sep=" -> ")
+
 # Print with custom end
 print("This is the first line.", end=" ")
 print("This is on the same line because of 'end'.")
+
+# Demonstrating end argument with different endings
+print("Line 1", end=" | ")
+print("Line 2", end="\n\n")  # Add double newlines
 
 # Multi-line print using triple quotes
 print("""This is a 
 multi-line 
 string print.""")
 
+# Demonstrating flush argument to force immediate output
+import time
+for i in range(3):
+    print(f"Progress: {i + 1}/3", flush=True, end="\r")
+    time.sleep(1)
+print("Done!", flush=True)
+
 # Print output to a file
 with open("output.txt", "w") as file:
     print("This is written to a file.", file=file)
+
+# Demonstrating file argument by writing multiple lines to another file
+with open("log.txt", "w") as file:
+    print("Log Entry 1: System started.", file=file)
+    print("Log Entry 2: System is running.", file=file)
+
---
+++ ch1/type.py
@@ -0,0 +1,25 @@
+def type_examples():
+    # Example with an integer
+    num = 42
+    print("Type of num:", type(num))
+
+    # Example with a string
+    txt = "Hello"
+    print("Type of txt:", type(txt))
+
+    # Example with a list
+    lst = [1, 2, 3]
+    print("Type of lst:", type(lst))
+
+    # Example with a dictionary
+    dct = {"key": "value"}
+    print("Type of dct:", type(dct))
+
+    # Example with a custom object
+    class Sample:
+        pass
+
+    obj = Sample()
+    print("Type of obj:", type(obj))
+
+type_examples()
